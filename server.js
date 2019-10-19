52f6e17152954b5a002cbf8e43597cd725bc9cc3
桑洪旭 <sanghongxv@163.com>
2019-10-19 16:44:58 +0800

    修改mongodb连接

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const applte = require("./routes/api/applte");
 // DB config
 const db = require("./config/index").mongoUrl;
 // 上传
-app.use('/upload',express.static('upload'));
+app.use('/upload', express.static('upload'));
 
 // 使用body-parser中间件
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect(db)
-        .then(() => console.log("Mongodb 连接成功"))
-        .catch(err => console.log(err));
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Mongodb 连接成功"))
+    .catch(err => console.log(err));
 
 // passport初始化
 app.use(passport.initialize());
@@ -25,15 +25,15 @@ require("./config/passport")(passport);
 
 // 配置apidoc
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/apidoc',function(req,res){
+app.get('/apidoc', function (req, res) {
     res.sendfile("./public/apidoc/index.html")
 })
 
 // 使用routes
-app.use("/api/applte",applte);
+app.use("/api/applte", applte);
 
 const port = process.env.POST || 5000;
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`运行的端口号为 ${port}`)
 })
