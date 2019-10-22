5a4acec87168ae8949c7911b83f6a17edb665ed2
桑洪旭 <sanghongxv@163.com>
2019-10-22 18:00:24 +0800

    no commit message

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+/*
+ * @Description: -
+ * @Author: sanghx
+ * @Date: 2019-10-18 14:35:30
+ * @LastEditors: sanghx
+ * @LastEditTime: 2019-10-22 17:59:33
+ */
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport")
 const app = express();
 
-const applte = require("./routes/api/applte");
+const user = require("./routes/api/user");
+const rss = require("./routes/api/rss");
 // DB config
 const db = require("./config/index").mongoUrl;
-// 上传
-app.use('/upload', express.static('upload'));
 
 // 使用body-parser中间件
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,17 +30,18 @@ app.use(passport.initialize());
 // 配置passport
 require("./config/passport")(passport);
 
-// 配置apidoc
-app.use(express.static(path.join(__dirname, 'public')))
-app.get('/apidoc', function (req, res) {
-    res.sendfile("./public/apidoc/index.html")
-})
+// // 配置apidoc
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.get('/apidoc',function(req,res){
+//     res.sendfile("./public/apidoc/index.html")
+// })
 
 // 使用routes
-app.use("/api/applte", applte);
+app.use("/api/user", user);
+app.use("/api/rss", rss);
 
 const port = process.env.POST || 5000;
 
 app.listen(port, () => {
-    console.log(`运行的端口号为 ${port}`)
+    console.log(`运行的端口号:${port}`)
 })
