ccb1e24a33fee0cbdb6aa18901ee465e1a60e61c
小孩 <sanghongxv@163.com>
2020-05-18 17:07:08 +0800

    update server.js.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport")
+const tools = require('./utils/tools.js')
+
 const app = express();
 
 const user = require("./routes/api/user");
@@ -18,6 +20,7 @@ const rss = require("./routes/api/rss");
 const db = require("./config/index").mongoUrl;
 
 // 使用body-parser中间件
+app.use('/', tools)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
